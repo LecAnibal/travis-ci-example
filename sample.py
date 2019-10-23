9857154805ed20683a0ea591a1b837cf395b5a32
Anibal Santiago <anibsanmor@gmail.com>
2019-10-23 16:32:01 -0500

    travis example

---
+++ sample.py
@@ -0,0 +1,14 @@
+
+from num2words import num2words
+
+def add_and_one(a,b):
+    return a+b+1
+
+def double(a):
+    return 2*a
+
+def say_hello(a):
+    return "hello {}".format(a)
+
+def num_speak(a):
+    return num2words(a)
